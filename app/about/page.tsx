0dcbdde985f066c3607716a49b3f6cdad25269a0
Jerica <jericahanna04@gmail.com>
2025-03-21 02:16:55 -0500

    adding to my about page, contact page, and home page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,6 +3,9 @@ export default function About() {
       <div>
         <h1>About Us</h1>
         <p>Learn more about our company and mission!</p>
+        <p>We are here to improve technology as we speak!</p>
+        <p>We love our customers</p>
+        <p>We love our work!</p>
       </div>
     );
   }
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to the Home Page!</h1>
-      <p>This is the main page of the website. Add more content here!</p>
+      <h1>Welcome to the Home Page for my Webscripting class!</h1>
+      <p>This is the main page for this project.</p>
+      <p>have fun, and enjoy yourself</p>
     </div>
   );
 }
