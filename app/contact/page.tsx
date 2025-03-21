551948092327dd6656467b72532b67a0bbe950e0
Jerica <jericahanna04@gmail.com>
2025-03-21 02:18:39 -0500

    improving my pages to look better and small changes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,18 +3,17 @@ export default function Contact() {
       <div>
         <h1>Contact Us</h1>
         <p>If you have any questions, feel free to reach out to us!</p>
-        <img src="/contact-image.jpg" alt="Contact Image" width="300" />
         <form>
           <div>
-            <label htmlFor="name">Name:</label>
+            <label>Name:</label>
             <input type="text" id="name" name="name" />
           </div>
           <div>
-            <label htmlFor="email">Email:</label>
+            <label>Email:</label>
             <input type="email" id="email" name="email" />
           </div>
           <div>
-            <label htmlFor="message">Message:</label>
+            <label>Message:</label>
             <textarea id="message" name="message"></textarea>
           </div>
           <button type="submit">Send</button>
